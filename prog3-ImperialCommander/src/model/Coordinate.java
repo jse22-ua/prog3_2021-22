31ac11d39f27af7b9a1c52272e9e0d483019a51d
jse22-ua <jse22@alu.ua.es>
2021-09-24 13:03:46 +0200

    I try it

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -21,26 +21,64 @@ public class Coordinate {
 	        y = c.y;
 	    }
 
-	    public int getX() {return x; }
-	    public int getY() { return y; }
+	    
+	    public int getX() {
+			return x;
+		}
 
-	    public String to_string(){
+		public void setX(int x) {
+			this.x = x;
+		}
+
+	    public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public String to_string(){
 	        StringBuilder concatenation=new StringBuilder();
 	        concatenation.append("["+ x + "," + y + "]");
 	        return concatenation.toString();
 	    }
+	    
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
 
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Coordinate other = (Coordinate) obj;
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
 	    public Coordinate add(Coordinate c){
 	        Coordinate c_new = new Coordinate(x+c.x,y+c.y);
-
+	        
 	        return c_new;
 
 	    }
 	    
 
 	    public Coordinate add(int x,int y){
-	        Coordinate c_new = new Coordinate(this.x+x, this.y+y);  // nuevamente hay que usar this->
-	                                            // porque los nombres coinciden 
+	        Coordinate c_new = new Coordinate(this.x+x, this.y+y);
+	                                           
 	        return c_new;
 	    }
+
 }
